d0a60ebf85d823e0865db13b93cee3e1604189e4
Paulhenry Saux <paulhenrysaux@gmail.com>
2026-08-02 13:29:14 +0200

    add new logout protocol

--- src/routes/logout.ts
+++ src/routes/logout.ts
@@ -33,7 +33,8 @@ router.get("/", csrfProtection, (req, res, next) => {
         csrfToken: req.csrfToken(),
         challenge: challenge,
         action: urljoin(process.env.BASE_URL || "", "/logout"),
-        webmailDomain: process.env.WEBMAIL_DOMAIN_WP
+        webmailDomain: process.env.WEBMAIL_DOMAIN_WP,
+        cryptpadDomain: process.env.CRYPTPAD_DOMAIN_WP,
       })
     })
     // This will handle any error that happens when making HTTP calls to hydra
