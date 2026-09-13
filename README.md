4e226251116354b2527aa664de901fe074b3bf4b
Paulhenry Saux <paulhenrysaux@gmail.com>
2026-09-13 10:33:49 +0200

    feat: logout with external SSO provider

--- README.md
+++ README.md
@@ -28,6 +28,7 @@ CORE_API_INTERNAL_SECRET=ezfeoizrourefgzerrvettyojuipytfdesdzstrye
 EXTERNAL_OIDC_ISSUER=https://sso.domain.fr
 EXTERNAL_OIDC_CLIENT_ID=client_id
 EXTERNAL_OIDC_CLIENT_SECRET=client_secret
+EXTERNAL_OIDC_LOGOUT_URI=https://sso.domain.fr/logout?from=aurion
 SESSION_SECRET=shshsshshshshze747
 # Use the /conf endpoint to get a valid OPAQUE_SERVER_SETUP value
 OPAQUE_SERVER_SETUP='bad_value'
--- src/routes/logout.ts
+++ src/routes/logout.ts
@@ -67,6 +67,11 @@ router.post("/", csrfProtection, (req, res, next) => {
       if (isJson) {
         return res.json({ success: true, redirect_to: String(redirect_to) })
       }
+      if(process.env.EXTERNAL_OIDC_LOGOUT_URI) {
+        const redirectUrl = new URL(process.env.EXTERNAL_OIDC_LOGOUT_URI)
+        redirectUrl.searchParams.set('from_aurion', 'true')
+        return res.redirect(String(redirectUrl))
+      }
       res.redirect(String(redirect_to))
     })
     .catch(next)
