e4557803265eb43fe023df2cae08503c2ec5c4b1
Paulhenry Saux <paulhenrysaux@gmail.com>
2026-09-18 08:33:21 +0200

    fix(external SSO): ensure user is redirected to external oidc logout uri when filled

--- src/routes/logout.ts
+++ src/routes/logout.ts
@@ -71,13 +71,13 @@ router.post("/", csrfProtection, (req, res, next) => {
          const FullredirectUrl = new URL(process.env.EXTERNAL_OIDC_FULL_LOGOUT_URI)
         FullredirectUrl.searchParams.set('from_aurion', 'true')
         return res.redirect(String(FullredirectUrl))
-      }
-      if(process.env.EXTERNAL_OIDC_LOGOUT_URI) {
+      } else if(process.env.EXTERNAL_OIDC_LOGOUT_URI) {
         const redirectUrl = new URL(process.env.EXTERNAL_OIDC_LOGOUT_URI)
         redirectUrl.searchParams.set('from_aurion', 'true')
         return res.redirect(String(redirectUrl))
-      }
+      } else {
       res.redirect(String(redirect_to))
+      }
     })
     .catch(next)
 })
